1265e77b048984be7c60c89d38231272b25a8bf0
Luis Costa <scluispaulo@gmail.com>
2025-01-25 19:29:50 -0300

    Add FileProcessorTests

---
+++ SmartVault.Test/FileProcessorTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Moq;
+using SmartVault.Program;
+using Xunit;
+
+namespace SmartVault.Test
+{
+    public class FileProcessorTests
+    {
+        [Fact]
+        public void FileProcessor_NotAuthenticated_DoesNotProcessFiles()
+        {
+            var mockOAuthIntegration = new Mock<IOAuthIntegration>();
+            mockOAuthIntegration.Setup(o => o.IsAuthenticated).Returns(false);
+
+            var fileProcessor = new FileProcessor(mockOAuthIntegration.Object);
+
+            var exception = Assert.Throws<UnauthorizedAccessException>(() => fileProcessor.GetAllFileSizes());
+            Assert.Equal("Not authenticated", exception.Message);
+        }
+    }
+}
